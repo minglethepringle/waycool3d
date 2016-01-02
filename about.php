c7b7bdfe3e4e0e2094d1fd65ee110e8e760fcb0a
wokman10 <walker.whitehouse@gmail.com>
2016-01-02 10:13:13 -0500

    Added about.php and imgs as well as minot changes to index.php

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>WayCool3D | About</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Google Font CSS-->
+    <link href='https://fonts.googleapis.com/css?family=Kanit:300' rel='stylesheet' type='text/css'>
+
+    <!-- Custom CSS -->
+    <link href="css/small-business.css" rel="stylesheet">
+    <link href="css/common.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <?php include './includes/nav.php';?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Call to Action Well -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="well text-center">
+                    This is a well that is a great spot for a business tagline or phone number for easy access!
+                </div>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Who are we?</h2>
+                <p>We are a small company consisting of 2 people located in Boston, Massachusetts</p>
+            </div>
+        </div>
+	<div class="row">
+	   <div class="col-md-4">
+		<h2>Mingle</h2>
+		<img src="./img/mingle.gif" alt="Mingle">
+	   </div>
+           <div class="col-md-4">
+                <h2>Walker</h2>
+                <img src="./img/walker.gif" alt="Walker">
+           </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+        <?php include("./includes/footer.php"); ?> 
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="index.php">
+                    <img src="img/logo.png" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="./about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="./shop.php">Shop</a>
+                    </li>
+		            <li>
+			            <a href="./contacts.php">Contacts</a>
+		            </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
