73840fb5f07d4f33aec7fed82f0816c06a0393d1
wokman10 <walker.whitehouse@gmail.com>
2016-01-03 09:05:49 -0500

    Added info on about.php

--- about.php
+++ about.php
@@ -39,10 +39,10 @@
     <!-- Page Content -->
     <div class="container">
         <!-- Content Row -->
-        <div class="row">
+        <div class="row" id="who">
             <div class="col-lg-12">
                 <h2>Who are we?</h2>
-                <p>We are a small company consisting of 2 people located in Boston, Massachusetts</p>
+                <p>We are a small company consisting of 2 extraoidinary middle schools who like to create and learn. We get straight A's and are located in Boston, Massachusetts</p>
             </div>
         </div>
 	<div class="row">
@@ -51,16 +51,23 @@
 		<a href="https://plus.google.com/u/0/+MingleLiakaEpicGamingPro/posts">
 		<img src="./img/mingle.gif" alt="Mingle">
 		</a>
-		<p style="font-size:20px">put some stuff about you her kid</p>
+		<p>put some stuff about you her kid</p>
 	   </div>
            <div class="col-md-4">
                 <h2>Walker</h2>
 		<a href="https://plus.google.com/u/0/107473445451148577968/about">
                 <img src="./img/walker.gif" alt="Walker">
 		</a>
-		<p style="font-size:20px">Walker is a middle schools student with a 3D printer and a computer. He likes to use the terminal and solve rubiks cubes. He can solve them in less then a minute and can solve 2x2's and pyraminxes. He also plays has been playing piano and cello for over 5 years.</p>
+		<p>Walker is a middle schools student with a 3D printer and a computer. He likes to use the terminal and solve rubiks cubes. He can solve them in less then a minute and can solve 2x2's and pyraminxes. He also plays has been playing piano and cello for over 5 years.</p>
            </div>
         </div>
+	<div class="row" id="What">
+	   <div class="col-lg-12">
+		<h2>What do we do?</h2>
+		<p>We use Walker's printer to print phone cases. Walker has a 3rd gen Cube printer. We also can print keychains or if you want we can make something you design in tinkercad or blender. All you need to do is send us the <i>.stl</i> file and we can make it. We charge based on how much the thing you make weighs.</p>
+	  </div>
+	</div>
+
         <!-- /.row -->
 
         <!-- Footer -->
--- index.php
+++ index.php
@@ -58,36 +58,24 @@
         <!-- /.row -->
 
         <hr>
-
-        <!-- Call to Action Well -->
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="well text-center">
-                    This is a well that is a great spot for a business tagline or phone number for easy access!
-                </div>
-            </div>
-            <!-- /.col-lg-12 -->
-        </div>
-        <!-- /.row -->
-
         <!-- Content Row -->
         <div class="row">
             <div class="col-md-4">
                 <h2>Who are we?</h2>
                 <p>We're just two ordinary middle-school entrepreneurs. With some extraordinary technology powers in our hands.</p>
-                <a class="btn btn-default" href="./about.php">More About Us</a>
+                <a class="btn btn-default" href="./about.php#who">More About Us</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <h2>What do we do?</h2>
                 <p>We use our 3D printer to print objects, from keychains to phone cases. We can also ship (with a charge, of course)!</p>
-                <a class="btn btn-default" href="#">More About Our 3D Printer</a>
+                <a class="btn btn-default" href="./about.php#what">More About Our 3D Printer</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <h2>Where do we do it?</h2>
                 <p>The webpage itself is stored on our web server. And the 3D printer is just sitting in Walker's basement, ready for orders.</p>
-                <a class="btn btn-default" href="#">More About How We Started</a>
+                <a class="btn btn-default" href="./about.php#where">More About How We Started</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
