ddde42fd7666befc5acb56d1f830f9bac49a177d
wokman10 <walker.whitehouse@gmail.com>
2016-01-03 14:55:42 -0700

    Stuff

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php define('_OK_INC',1);
 
-// error handling code 
+// errors
 error_reporting(E_ALL^E_STRICT);
 ini_set("display_errors", 1);
 ini_set("log_errors", 1);
-ini_set("error_log", dirname($_SERVER['DOCUMENT_ROOT'])."./err/php.err");
+ini_set("error_log", dirname($_SERVER['DOCUMENT_ROOT'])."/_log_files/waycool3d_phperrors.log");
 
+// requests
 $req = "/home";
-
-// incoming requests
 if((isset($_SERVER['REQUEST_URI'])) && (preg_match('/^\/[A-Za-z0-9-_\/]+$/',($_SERVER['REQUEST_URI'])))) {
     $req = rtrim($_SERVER['REQUEST_URI'],"/");
-    
+var_dump($req);die(meh);    
     if(!file_exists($_SERVER['DOCUMENT_ROOT'].'./pages'.$req.'.php')) {
         $req = "/home";
     }
 }
-// adds data file array with sensible default meta tags
+// metadata
 @include_once(realpath(__DIR__)).'./pages/data'.$req.'.php';
 if(!isset($meta)) {
     $meta = array(
@@ -28,6 +27,6 @@ if(!isset($meta)) {
 
 require_once(realpath(__DIR__)).'/inc/header.php';
 require_once(realpath(__DIR__)).'/inc/nav.php';
-require_once(realpath(__DIR__)).'/pages'.$req.'.php'; // actual page content
+require_once(realpath(__DIR__)).'/pages/'.$req.'.php'; // actual page content
 require_once(realpath(__DIR__)).'/inc/footer.php';
 ?>
