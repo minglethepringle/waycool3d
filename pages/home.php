683fbeeb718a7acb88fc25a047bc5b5fd76207e1
Mingle Li <limingle5@gmail.com>
2016-01-03 21:02:30 -0500

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -13,7 +13,7 @@
             <div class="col-md-4">
                 <h1>WayCool3D</h1>
                 <p>We print stuff. Not like inkjet or laser printing, but 3D printing. It adds another whole new dimension, and it's possible to print basically anything you want! Since this breakthrough technology gives printers a whole new dimension, your own 3D printed objects are just a click ahead.</p>
-                <a class="btn btn-primary btn-lg" href="./shop.php">Check out our products!</a>
+                <a class="btn btn-primary btn-lg" href="/shop.php">Check out our products!</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -25,19 +25,19 @@
             <div class="col-md-4">
                 <h2>Who are we?</h2>
                 <p>We're just two ordinary middle-school entrepreneurs. With some extraordinary technology powers in our hands.</p>
-                <a class="btn btn-default" href="./about.php#who">More About Us</a>
+                <a class="btn btn-default" href="/about#who">More About Us</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <h2>What do we do?</h2>
                 <p>We use our 3D printer to print objects, from keychains to phone cases. We can also ship (with a charge, of course)!</p>
-                <a class="btn btn-default" href="./about.php#what">More About Our 3D Printer</a>
+                <a class="btn btn-default" href="/about#what">More About Our 3D Printer</a>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <h2>Where do we do it?</h2>
                 <p>The webpage itself is stored on our web server. And the 3D printer is just sitting in Walker's basement, ready for orders.</p>
-                <a class="btn btn-default" href="./about.php#where">More About How We Started</a>
+                <a class="btn btn-default" href="/about#where">More About How We Started</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
