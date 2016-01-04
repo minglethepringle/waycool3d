7894100d54f5d551ea5302fa6ea63e42c8b0468c
Mingle Li <limingle5@gmail.com>
2016-01-03 20:55:52 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -25,7 +25,7 @@
     <!-- Custom CSS -->
     <link href="css/small-business.css" rel="stylesheet">
     <link href="css/common.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
