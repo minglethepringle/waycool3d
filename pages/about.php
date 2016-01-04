696d9081e9206afe53e7e04dcff0945e3a41237b
Mingle Li <limingle5@gmail.com>
2016-01-04 10:47:40 -0500

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -1,32 +1,35 @@
     <!-- Page Content -->
     <div class="container">
         <!-- Content Row -->
-        <div class="row" id="who">
+        <a id="who"></a>
+        <div class="row">
             <div class="col-lg-12">
                 <h2>Who are we?</h2>
-                <p>We are a small company consisting of 2 extraoidinary middle schools who like to create and learn. We get straight A's and are located in Boston, Massachusetts</p>
+                <p>We are a small startup company consisting of 2 middle school entrepreneurs who like to create and learn.</p>
             </div>
         </div>
 	<div class="row">
 	   <div class="col-md-4">
 		<h2>Mingle</h2>
 		<a href="https://plus.google.com/u/0/+MingleLiakaEpicGamingPro/posts">
-		<img src="./img/mingle.gif" alt="Mingle">
+			<img src="./img/mingle.gif" alt="Mingle">
 		</a>
-		<p>put some stuff about you her kid</p>
+		<p></p>
 	   </div>
            <div class="col-md-4">
                 <h2>Walker</h2>
 		<a href="https://plus.google.com/u/0/107473445451148577968/about">
-                <img src="./img/walker.gif" alt="Walker">
+                	<img src="./img/walker.gif" alt="Walker">
 		</a>
-		<p>Walker is a middle school student with a 3D printer and a computer. He likes to use the terminal and solve rubiks cubes. He can solve them in less then a minute and can solve 2x2's and pyraminxes. He also plays has been playing piano and cello for over 5 years.</p>
+		<p>Mingle is the software and client side half of the company. He is the person who makes the layout, page, and shop plugin for the webpage. He also started the GitHub repository and taught Walker how to use Git./p>
+		<p>Walker is the hardware and server side half of the company. He is the person who prints the objects and codes the PHP rules for the server and webpage to use. He also helped with the front-end website. </p>
            </div>
         </div>
-	<div class="row" id="What">
+        <a id="what"></a>
+	<div class="row">
 	   <div class="col-lg-12">
 		<h2>What do we do?</h2>
-		<p>We use Walker's printer to print phone cases. Walker has a 3rd gen Cube printer. We also can print keychains or if you want we can make something you design in tinkercad or blender. All you need to do is send us the <i>.stl</i> file and we can make it. We charge based on how much the thing you make weighs.</p>
+		<p>Using our 3rd generation Cube 3D printer from 3D Systems, we print whatever you order. We use non-toxic, environmentally friendly PLA plastic, which is high quality and prints in the highest resolution for a better product.</p>
 		<img src="/img/cube_printr" alt="3D printer">
 	  </div>
 	</div>
