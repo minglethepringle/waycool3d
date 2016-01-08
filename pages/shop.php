f8f98f3544c449e364385c4078e554f617111269
wokman10 <walker.whitehouse@gmail.com>
2016-01-07 18:39:40 -0700

    Started work on shop

---
+++ pages/shop.php
@@ -0,0 +1,17 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+        		<h1>Products</h1>
+        	</div>
+	</div>
+	<div class="row">
+		<hr>
+        	<div id="case" class="col-md-4 box">
+		<h3>Phone case</h3>
+		
+		</div>
+		<div class="col-md-4">
+                </div>
+        </div>
+
+</div>
