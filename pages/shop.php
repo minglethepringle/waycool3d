5d6eb9e7d1dc2b67560c7c0e990d86ea74a9fe10
wokman10 <walker.whitehouse@gmail.com>
2016-01-07 19:41:24 -0700

    Removed banner

--- pages/shop.php
+++ pages/shop.php
@@ -1,41 +1,4 @@
     <div class="container">
-
-        <div class="row">
-	    <div class="col-md-3">
-	    </div>
-            <div class="col-md-9">
-		   
-                <div class="row carousel-holder">
-
-                    <div class="col-md-12">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="./img/randy.jpg" alt="">
-                                </div>
-                                <div class="item">
-				    <img class="slide-image" src="./img/sam.jpg" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-
                 <div class="row">
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
