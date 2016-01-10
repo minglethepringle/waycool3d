1a62ee435c51ad70693721940e7b0af5d7d7059b
wokman10 <walker.whitehouse@gmail.com>
2016-01-09 18:58:31 -0700

    Added dogs

--- pages/shop.php
+++ pages/shop.php
@@ -3,7 +3,8 @@
 
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="img/cases_example.jpg" alt="">
+			    <hr>
                             <div class="caption">
                                 <h4 class="pull-right">$10</h4>
                                 <h4><a href="#">Custom Case</a>
@@ -16,11 +17,25 @@
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
                             <img src="img/cats.jpg" alt="">
+			    <hr>
                             <div class="caption">
                                 <h4 class="pull-right">$5</h4>
                                 <h4><a href="#">Cudding Cats!</a>
                                 </h4>
-                                <p>Cats are cool. Especially cudding cats. <a href="/shop/cats">If you want one come and get it!</a></p>
+                                <p>Cats are cool. Especially cudding cats. <a href="/shop/cats">You like cats, Right?</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="img/dogs.jpg" alt="">
+                            <hr>
+                            <div class="caption">
+                                <h4 class="pull-right">$5</h4>
+                                <h4><a href="#">Sleeping Dogs!</a>
+                                </h4>
+                                <p>Dogs are good too. <a href="/shop/cats">You want one?</a></p>
                             </div>
                         </div>
                     </div>
