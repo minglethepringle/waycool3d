516016f6029fbdf8c3ed5b5eb94d7238798fed51
wokman10 <walker.whitehouse@gmail.com>
2016-01-12 13:01:38 -0700

    Fixed indentation & made btn on shop

--- inc/nav.php
+++ inc/nav.php
@@ -1,40 +1,35 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <a class="navbar-brand" href="./home">
-                    <img src="img/logo.png" alt="">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="./about">About</a>
-                    </li>
-                    <li>
-                        <a href="./shop">Shop</a>
-                    </li>
-                    <li>
-                        <a href="./faq">FAQ</a>
-                    </li>
-                    <li>
-                        <a href="./story">Our Story</a>
-                    </li>
-		    <li>
-			<a href="./contacts.php">Contact Us</a>
-		    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="./home">
+        <img src="img/logo.png" alt="">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+        <li>
+          <a href="/faq">FAQ</a>
+        </li>
+        <li>
+          <a href="/story">Our Story</a>
+        </li>
+        <li>
+          <a href="/contacts">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
