61d4446a4020e5abc1035d0a6e463a7bca7803e6
wokman10 <walker.whitehouse@gmail.com>
2016-01-26 08:59:09 -0700

    Javascript goes at bottom

--- pages/shop.php
+++ pages/shop.php
@@ -14,7 +14,6 @@ function togglebig(el){
     el.className = "col-sm-4 col-lg-4 col-md-4";
   }
 };
-
 function toggledog(){
   toggle(cases);
   toggle(cat);
@@ -47,15 +46,19 @@ function toggledog(){
       </div>
       <div id="dog" class="col-sm-4 col-lg-4 col-md-4">
         <div class="thumbnail">
-          <img src="img/dogs.jpg" alt="Example of printed dogs">
-          <hr>
-          <div class="caption">
-            <h4 class="pull-right">$3</h4>
-            <h4>Sleeping Dog</h4>
-            <p>Here it is for you, if you're more of a dog person! Dogs are just so cute when sleeping.</p>
-            <button class="btn btn-primary" id="toggledog" onclick="toggledog()">Learn more</button>
-            <div id="hiddendog">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div class="row">
+            <div id="dogin" class="col-sm-12 col-lg-12 col-md-12">
+              <img src="img/dogs.jpg" alt="Example of printed dogs">
+              <hr>
+              <div class="caption">
+                <h4 class="pull-right">$3</h4>
+                <h4>Sleeping Dog</h4>
+                <p>Here it is for you, if you're more of a dog person! Dogs are just so cute when sleeping.</p>
+                <button class="btn btn-primary" id="toggledog" onclick="toggledog()">Learn more</button>
+              </div>
+            </div>
+            <div id="doghide" class="hidden">
+              <iframe src="https://docs.google.com/forms/d/1c9a5DCYuqkCMRVKI036pjlZp_vJ4X5xHeV1QAiuhbow/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
             </div>
           </div>
         </div>
