b5f63ceac9845ef2122e0698aadae66860a32b9c
Mingle <limingle5@gmail.com>
2016-02-03 10:06:01 -0500

    Updated main.js to js code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,51 @@
-$('#toggledog').click(function(){
-    var val = $(this).val();
-    $('#hiddendog').slideToggle();
-});
+function toggle(el){ 
+  if (el.className == "col-sm-4 col-lg-4 col-md-4"){
+    el.className = "hidden";      
+  } else {
+    el.className = "col-sm-4 col-lg-4 col-md-4";
+  }
+};
+
+function toggleform(el){
+  if (el.className == "col-sm-8 col-lg-8 col-md-8"){
+    el.className = "hidden";
+  } else {
+    el.className = "col-sm-8 col-lg-8 col-md-8";
+  }
+};
+            
+function togglebig(el){
+  if (el.className == "col-sm-4 col-lg-4 col-md-4"){
+    el.className = "col-sm-12 col-lg-12 col-md-12";
+  } else {  
+    el.className = "col-sm-4 col-lg-4 col-md-4";
+  }
+};
+
+function togglecase(){
+  toggle(cat);
+  toggle(dog);
+  toggle(panda);
+  toggleform(hcase);
+};
+
+function togglecat(){
+  toggle(cases);
+  toggle(dog);
+  toggle(panda);
+  toggleform(hcat);
+};
+
+function toggledog(){
+  toggle(cases);
+  toggle(cat);
+  toggle(panda);
+  toggleform(hdog);
+};
+
+function togglepanda(){
+  toggle(cases);
+  toggle(cat);
+  toggle(dog);
+  toggleform(hpanda);
+};
