f8367ad086a2b5fe7e84b0ce71620869a7a2c403
Mingle <limingle5@gmail.com>
2016-02-03 10:05:18 -0500

    Added main.js back

--- inc/header.php
+++ inc/header.php
@@ -20,6 +20,7 @@
   <link href="./fonts/fontawesome/css/font-awesome.min.css" rel="stylesheet">
   <link href="./css/common.css" rel="stylesheet">
   <link href="./css/main.css" rel="stylesheet">
+  <script src="./js/main.js"></script>
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
