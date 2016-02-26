fce04540203c37c0f4b5216d3f6ca68b3dc0f9b1
Mingle <limingle5@gmail.com>
2016-02-25 19:23:32 -0500

    Update pull.php

--- pages/pull.php
+++ pages/pull.php
@@ -1,3 +1,18 @@
 <?php
 exec('git pull')
 ?>
+<div class="container" style="text-align: center">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Successfully pulled from the <a href="https://github.com/iiEpixKittai/waycool3d">GitHub repository</a>.</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12">
+        <h3>Redirecting to the home page...</h1>
+      </div>
+    </div>
+</div>
+<script>
+  setTimeout(function(){window.location.replace("http://waycool3d.com");}, 3000);
+</script>
