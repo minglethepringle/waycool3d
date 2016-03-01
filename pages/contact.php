24978aaea28960c1d78950acbf724413ada335a5
Mingle <limingle5@gmail.com>
2016-03-01 08:42:28 -0500

    Create contact.php

---
+++ pages/contact.php
@@ -0,0 +1,7 @@
+<div class="container" style="text-align: center">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>This page is still under development.</h1>
+      </div>
+    </div>
+</div>
